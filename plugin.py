8b4ccb3fef7ab9d67cd3e7a8b731a23170b6807b
doabigcheese <59741556+doabigcheese@users.noreply.github.com>
2022-05-09 13:38:06 +0200

    prototype

--- plugin.py
+++ plugin.py
@@ -1,34 +1,92 @@
 import TouchPortalAPI as TP
+import json
+import os
+import sys
+
 #required: pip install TouchPortal-API
 #required: python v3.8
 
 # Setup callbacks and connection
 TPClient = TP.Client("SCNav")
 
+planetsListPointer = 0
+poiListPointer = 0
+
+#setup DATA, Database.json from valalol's sc navi tool https://github.com/Valalol/Star-Citizen-Navigation/releases
+with open('Database.json') as f:
+    Database = json.load(f)
+Container_list = []
+for i in Database["Containers"]:
+    Container_list.append(Database["Containers"][i]["Name"])
+Planetary_POI_list = {}
+for container_name in Database["Containers"]:
+    Planetary_POI_list[container_name] = []
+    for poi in Database["Containers"][container_name]["POI"]:
+        if "OM-" not in poi and "Comm Array" not in poi: 
+          Planetary_POI_list[container_name].append(poi)
+        
+            
 # This event handler will run once when the client connects to Touch Portal
 @TPClient.on(TP.TYPES.onConnect) # Or replace TYPES.onConnect with 'info'
 def onStart(data):
     print("Connected!", data)
     # Update a state value in TouchPortal
     TPClient.stateUpdate("SCNavState", "Connected!")
+    TPClient.stateUpdate ("selectedPlanet", Container_list[0])
 
 # Action handlers, called when user activates one of this plugin's actions in Touch Portal.
 @TPClient.on(TP.TYPES.onAction) # Or 'action'
+
 def onAction(data):
+    global planetsListPointer,poiListPointer,Container_list, Planetary_POI_list
     print(data)
     # do something based on the action ID and the data value
-    if data['actionId'] == "SelectDstPlanet":
+    
+    if data['actionId'] == "UpPlanet":
       # get the value from the action data (a string the user specified)
-      #action_value = getActionDataValue(data.get('data'), 'ExampleTextData')
-      #print(action_value)
-      # We can also update our ExampleStates with the Action Value
-      TPClient.stateUpdate("SCNavState", "Planet selected")
-    if data['actionId'] == "SelectPOIonPlanet":
+      if planetsListPointer > 0: 
+        planetsListPointer -= 1
+        print("UpPlanet ", str(planetsListPointer))
+       
+        TPClient.stateUpdate("selectedPlanet",  Container_list[planetsListPointer])
+        TPClient.stateUpdate("selectedPOI", Planetary_POI_list[Container_list[planetsListPointer]][0] )
+        poiListPointer = 0
+        
+    if data['actionId'] == "DownPlanet":
       # get the value from the action data (a string the user specified)
-      action_value = getActionDataValue(data.get('data'), 'ExampleTextData2')
-      print(action_value)
-      # We can also update our ExampleStates with the Action Value
-      TPClient.stateUpdate("SCNavState", action_value)  
+      if planetsListPointer < len(Container_list)-1:
+        planetsListPointer += 1
+        print("DownPlanet ", str(planetsListPointer))
+        #TPClient.stateUpdate ("selectedPlanet", "Daymar")
+        TPClient.stateUpdate("selectedPlanet",  Container_list[planetsListPointer])
+        TPClient.stateUpdate("selectedPOI", Planetary_POI_list[Container_list[planetsListPointer]][0] )
+        poiListPointer = 0
+                         
+    if data['actionId'] == "UpPoiName":
+      # get the value from the action data (a string the user specified)
+      if poiListPointer > 0:
+        poiListPointer -= 1
+        print("DownPoiName ", str(poiListPointer))
+        TPClient.stateUpdate("selectedPOI", Planetary_POI_list[Container_list[planetsListPointer]][poiListPointer] )
+        
+        
+    if data['actionId'] == "DownPoiName":
+      # get the value from the action data (a string the user specified)
+      if poiListPointer < len(Planetary_POI_list[Container_list[planetsListPointer]])-1:
+        poiListPointer += 1
+        print("DownPoiName ", str(poiListPointer))
+        TPClient.stateUpdate("selectedPOI", Planetary_POI_list[Container_list[planetsListPointer]][poiListPointer] )
+        
+        
+    if data['actionId'] == "startNav":
+      # get the value from the action data (a string the user specified)
+      print("startNav for ", Container_list[planetsListPointer], ", ", Planetary_POI_list[Container_list[planetsListPointer]][poiListPointer] )
+    if data['actionId'] == "saveLocation":
+      # get the value from the action data (a string the user specified)
+      print("saveLocation")
+    if data['actionId'] == "updateLocation":
+      # get the value from the action data (a string the user specified)
+      print("updateLocation")
 
 # Shutdown handler, called when Touch Portal wants to stop your plugin.
 @TPClient.on(TP.TYPES.onShutdown) # or 'closePlugin'
